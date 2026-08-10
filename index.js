67a5101544da564cdb65ee48fa5cf8c05074a76f
Eric-0517 <0147zeo@gmail.com>
2026-08-10 12:32:45 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const { ShardingManager } = require('discord.js');
+const http = require('http');
 require('dotenv').config();
+
+
+const PORT = process.env.PORT || 3000;
+http
+  .createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+    res.end('Valorant Stats Discord Bot is running!');
+  })
+  .listen(PORT, () => {
+    console.log(`[Web Server] HTTP Web Server 已啟動於 Port: ${PORT}`);
+  });
+
 
 const manager = new ShardingManager('./bot.js', {
   totalShards: 'auto',
@@ -11,4 +24,4 @@ manager.on('shardCreate', (shard) => {
   console.log(`Launched shard ${shard.id}`);
 });
 
-manager.spawn().catch((e) => console.log(e));
+manager.spawn().catch((e) => console.log('[Sharding Error]:', e));
